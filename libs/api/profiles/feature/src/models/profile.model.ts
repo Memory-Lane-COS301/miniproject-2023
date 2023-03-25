abe787b64f09b2925ef011f8bf0033004b983792
u20612894 <u20612894@tuks.co.za>
2023-03-25 07:50:37 +0200

    Fixing linting errors
    A number of linting errors popped up in the previous commit. This is an attempt to fix them

--- libs/api/profiles/feature/src/models/profile.model.ts
+++ libs/api/profiles/feature/src/models/profile.model.ts
@@ -10,7 +10,7 @@ import {
     IProfile,
     OccupationDetailsUpdatedEvent,
     PersonalDetailsUpdatedEvent,
-     ProfileCreatedEvent,
+    ProfileCreatedEvent,
     ProfileStatus,
     ProfileStatusUpdatedEvent
 } from '@mp/api/profiles/util';
@@ -31,7 +31,7 @@ export class Profile extends AggregateRoot implements IProfile {
     public memoryCount?:number | null | undefined,
     public friendList?:string[] | null | undefined, 
     public recentPosts?: string[] | null | undefined, 
-    public created?: FirebaseFirestore.Timestamp | null | undefined
+    public created?: FirebaseFirestore.Timestamp | null |undefined
   ) {
     super();
   }
@@ -52,7 +52,7 @@ export class Profile extends AggregateRoot implements IProfile {
       profile.friendList,
       profile.recentPosts,
       profile.created
-    );
+    ); 
     return instance;
   }
 
@@ -60,183 +60,183 @@ export class Profile extends AggregateRoot implements IProfile {
     this.apply(new ProfileCreatedEvent(this.toJSON()));
   }
 
-  // updateAddressDetails(addressDetails: IAddressDetails) {
-  //   if (!this.addressDetails) this.addressDetails = {};
-  //   this.addressDetails.residentialArea = addressDetails.residentialArea
-  //     ? addressDetails.residentialArea
-  //     : this.addressDetails.residentialArea;
-  //   this.addressDetails.workArea = addressDetails.workArea
-  //     ? addressDetails.workArea
-  //     : this.addressDetails.workArea;
-  //   this.apply(new AddressDetailsUpdatedEvent(this.toJSON()));
-  // }
+  updateAddressDetails(addressDetails: IAddressDetails) {
+    if (!this.addressDetails) this.addressDetails = {};
+    this.addressDetails.residentialArea = addressDetails.residentialArea
+      ? addressDetails.residentialArea
+      : this.addressDetails.residentialArea;
+    this.addressDetails.workArea = addressDetails.workArea
+      ? addressDetails.workArea
+      : this.addressDetails.workArea;
+    this.apply(new AddressDetailsUpdatedEvent(this.toJSON()));
+  }
 
-  // updateContactDetails(contactDetails: IContactDetails) {
-  //   if (!this.contactDetails) this.contactDetails = {};
-  //   this.contactDetails.cellphone = contactDetails.cellphone
-  //     ? contactDetails.cellphone
-  //     : this.contactDetails.cellphone;
-  //   this.apply(new ContactDetailsUpdatedEvent(this.toJSON()));
-  // }
+  updateContactDetails(contactDetails: IContactDetails) {
+    if (!this.contactDetails) this.contactDetails = {};
+    this.contactDetails.cellphone = contactDetails.cellphone
+      ? contactDetails.cellphone
+      : this.contactDetails.cellphone;
+    this.apply(new ContactDetailsUpdatedEvent(this.toJSON()));
+  }
 
-  // updatePersonalDetails(personalDetails: IPersonalDetails) {
-  //   if (!this.personalDetails) this.personalDetails = {};
-  //   this.personalDetails.age = personalDetails.age
-  //     ? personalDetails.age
-  //     : this.personalDetails.age;
-  //   this.personalDetails.gender = personalDetails.gender
-  //     ? personalDetails.gender
-  //     : this.personalDetails.gender;
-  //   this.personalDetails.ethnicity = personalDetails.ethnicity
-  //     ? personalDetails.ethnicity
-  //     : this.personalDetails.ethnicity;
-  //   this.apply(new PersonalDetailsUpdatedEvent(this.toJSON()));
-  // }
+  updatePersonalDetails(personalDetails: IPersonalDetails) {
+    if (!this.personalDetails) this.personalDetails = {};
+    this.personalDetails.age = personalDetails.age
+      ? personalDetails.age
+      : this.personalDetails.age;
+    this.personalDetails.gender = personalDetails.gender
+      ? personalDetails.gender
+      : this.personalDetails.gender;
+    this.personalDetails.ethnicity = personalDetails.ethnicity
+      ? personalDetails.ethnicity
+      : this.personalDetails.ethnicity;
+    this.apply(new PersonalDetailsUpdatedEvent(this.toJSON()));
+  }
 
-  // updateOccupationDetails(occupationDetails: IOccupationDetails) {
-  //   if (!this.occupationDetails) this.occupationDetails = {};
-  //   this.occupationDetails.householdIncome = occupationDetails.householdIncome
-  //     ? occupationDetails.householdIncome
-  //     : this.occupationDetails.householdIncome;
-  //   this.occupationDetails.occupation = occupationDetails.occupation
-  //     ? occupationDetails.occupation
-  //     : this.occupationDetails.occupation;
-  //   this.apply(new OccupationDetailsUpdatedEvent(this.toJSON()));
-  // }
+  updateOccupationDetails(occupationDetails: IOccupationDetails) {
+    if (!this.occupationDetails) this.occupationDetails = {};
+    this.occupationDetails.householdIncome = occupationDetails.householdIncome
+      ? occupationDetails.householdIncome
+      : this.occupationDetails.householdIncome;
+    this.occupationDetails.occupation = occupationDetails.occupation
+      ? occupationDetails.occupation
+      : this.occupationDetails.occupation;
+    this.apply(new OccupationDetailsUpdatedEvent(this.toJSON()));
+  }
 
-  // updateAccountDetails(accountDetails: IAccountDetails) {
-  //   if (!this.accountDetails) this.accountDetails = {};
-  //   this.accountDetails.displayName = accountDetails.displayName
-  //     ? accountDetails.displayName
-  //     : this.accountDetails.displayName;
-  //   this.accountDetails.email = accountDetails.email
-  //     ? accountDetails.email
-  //     : this.accountDetails.email;
-  //   this.accountDetails.photoURL = accountDetails.photoURL
-  //     ? accountDetails.photoURL
-  //     : this.accountDetails.photoURL;
-  //   this.accountDetails.password = accountDetails.password
-  //     ? accountDetails.password
-  //     : this.accountDetails.password;
-  //   this.apply(new AccountDetailsUpdatedEvent(this.toJSON()));
-  // }
+  updateAccountDetails(accountDetails: IAccountDetails) {
+    if (!this.accountDetails) this.accountDetails = {};
+    this.accountDetails.displayName = accountDetails.displayName
+      ? accountDetails.displayName
+      : this.accountDetails.displayName;
+    this.accountDetails.email = accountDetails.email
+      ? accountDetails.email
+      : this.accountDetails.email;
+    this.accountDetails.photoURL = accountDetails.photoURL
+      ? accountDetails.photoURL
+      : this.accountDetails.photoURL;
+    this.accountDetails.password = accountDetails.password
+      ? accountDetails.password
+      : this.accountDetails.password;
+    this.apply(new AccountDetailsUpdatedEvent(this.toJSON()));
+  }
 
-  // private updateAccountDetailsStatus() {
-  //   if (!this.accountDetails) {
-  //     this.accountDetails = {};
-  //     this.accountDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+  private updateAccountDetailsStatus() {
+    if (!this.accountDetails) {
+      this.accountDetails = {};
+      this.accountDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   if (!this.accountDetails.displayName || !this.accountDetails.email) {
-  //     this.accountDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+    if (!this.accountDetails.displayName || !this.accountDetails.email) {
+      this.accountDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   this.accountDetails.status = ProfileStatus.COMPLETE;
-  //   return;
-  // }
+    this.accountDetails.status = ProfileStatus.COMPLETE;
+    return;
+  }
 
-  // private updateAddressDetailsStatus() {
-  //   if (!this.addressDetails) {
-  //     this.addressDetails = {};
-  //     this.addressDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+  private updateAddressDetailsStatus() {
+    if (!this.addressDetails) {
+      this.addressDetails = {};
+      this.addressDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   if (!this.addressDetails.residentialArea || !this.addressDetails.workArea) {
-  //     this.addressDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+    if (!this.addressDetails.residentialArea || !this.addressDetails.workArea) {
+      this.addressDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   this.addressDetails.status = ProfileStatus.COMPLETE;
-  //   return;
-  // }
+    this.addressDetails.status = ProfileStatus.COMPLETE;
+    return;
+  }
 
-  // private updateContactDetailsStatus() {
-  //   if (!this.contactDetails) {
-  //     this.contactDetails = {};
-  //     this.contactDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+  private updateContactDetailsStatus() {
+    if (!this.contactDetails) {
+      this.contactDetails = {};
+      this.contactDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   if (!this.contactDetails.cellphone) {
-  //     this.contactDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+    if (!this.contactDetails.cellphone) {
+      this.contactDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   this.contactDetails.status = ProfileStatus.COMPLETE;
-  //   return;
-  // }
+    this.contactDetails.status = ProfileStatus.COMPLETE;
+    return;
+  }
 
-  // private updatePersonalDetailsStatus() {
-  //   if (!this.personalDetails) {
-  //     this.personalDetails = {};
-  //     this.personalDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+  private updatePersonalDetailsStatus() {
+    if (!this.personalDetails) {
+      this.personalDetails = {};
+      this.personalDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   if (
-  //     !this.personalDetails.age ||
-  //     !this.personalDetails.gender ||
-  //     !this.personalDetails.ethnicity
-  //   ) {
-  //     this.personalDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+    if (
+      !this.personalDetails.age ||
+      !this.personalDetails.gender ||
+      !this.personalDetails.ethnicity
+    ) {
+      this.personalDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   this.personalDetails.status = ProfileStatus.COMPLETE;
-  //   return;
-  // }
+    this.personalDetails.status = ProfileStatus.COMPLETE;
+    return;
+  }
 
-  // private updateOccupationDetailsStatus() {
-  //   if (!this.occupationDetails) {
-  //     this.occupationDetails = {};
-  //     this.occupationDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+  private updateOccupationDetailsStatus() {
+    if (!this.occupationDetails) {
+      this.occupationDetails = {};
+      this.occupationDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   if (
-  //     !this.occupationDetails.householdIncome ||
-  //     !this.occupationDetails.occupation
-  //   ) {
-  //     this.occupationDetails.status = ProfileStatus.INCOMPLETE;
-  //     this.status = ProfileStatus.INCOMPLETE;
-  //     return;
-  //   }
+    if (
+      !this.occupationDetails.householdIncome ||
+      !this.occupationDetails.occupation
+    ) {
+      this.occupationDetails.status = ProfileStatus.INCOMPLETE;
+      this.status = ProfileStatus.INCOMPLETE;
+      return;
+    }
 
-  //   this.occupationDetails.status = ProfileStatus.COMPLETE;
-  //   return;
-  // }
+    this.occupationDetails.status = ProfileStatus.COMPLETE;
+    return;
+  }
 
-  // updateStatus() {
-  //   this.updateAccountDetailsStatus();
-  //   this.updateAddressDetailsStatus();
-  //   this.updateContactDetailsStatus();
-  //   this.updatePersonalDetailsStatus();
-  //   this.updateOccupationDetailsStatus();
+  updateStatus() {
+    this.updateAccountDetailsStatus();
+    this.updateAddressDetailsStatus();
+    this.updateContactDetailsStatus();
+    this.updatePersonalDetailsStatus();
+    this.updateOccupationDetailsStatus();
 
-  //   if (
-  //     this.accountDetails?.status === ProfileStatus.COMPLETE &&
-  //     this.addressDetails?.status === ProfileStatus.COMPLETE &&
-  //     this.contactDetails?.status === ProfileStatus.COMPLETE &&
-  //     this.personalDetails?.status === ProfileStatus.COMPLETE &&
-  //     this.occupationDetails?.status === ProfileStatus.COMPLETE
-  //   ) {
-  //     this.status = ProfileStatus.COMPLETE;
-  //   }
+    if (
+      this.accountDetails?.status === ProfileStatus.COMPLETE &&
+      this.addressDetails?.status === ProfileStatus.COMPLETE &&
+      this.contactDetails?.status === ProfileStatus.COMPLETE &&
+      this.personalDetails?.status === ProfileStatus.COMPLETE &&
+      this.occupationDetails?.status === ProfileStatus.COMPLETE
+    ) {
+      this.status = ProfileStatus.COMPLETE;
+    }
 
-  //   this.apply(new ProfileStatusUpdatedEvent(this.toJSON()));
-  // }
+    this.apply(new ProfileStatusUpdatedEvent(this.toJSON()));
+  }
 
   toJSON(): IProfile {
     return {
--- libs/api/users/util/src/interfaces/user.interface.ts
+++ libs/api/users/util/src/interfaces/user.interface.ts
@@ -3,11 +3,8 @@ import { Timestamp } from 'firebase-admin/firestore';
 export interface IUser {
   id: string;
   email: string | null | undefined;
-  //displayName?: string | null | undefined;
   username: string| null | undefined;
   photoURL: string | null | undefined;
- // phoneNumber?: string | null | undefined;
-  //customClaims?: { [key: string]: any } | null | undefined;
   loginStamp: Timestamp | null | undefined;
   created: Timestamp | null | undefined;
 }
