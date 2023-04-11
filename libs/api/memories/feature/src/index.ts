df5dc168728af30ae537f9402193e19ede362757
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-04-11 16:03:42 +0200

    made changes to base esling config file to enable local linting

--- libs/api/memories/feature/src/index.ts
+++ libs/api/memories/feature/src/index.ts
@@ -1,3 +1,3 @@
-export * from './memories.module';
-export * from './memories.sagas';
-export * from './memories.service';
+export * from './memories.module'
+export * from './memories.sagas'
+export * from './memories.service'
