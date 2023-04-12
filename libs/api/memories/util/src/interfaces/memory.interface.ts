229eb9e1ff737fd7a45086e526a153e253738342
AlistairMRoss <alistairmikeross@gmail.com>
2023-04-12 12:08:56 +0200

    fix standard

--- libs/api/memories/util/src/interfaces/memory.interface.ts
+++ libs/api/memories/util/src/interfaces/memory.interface.ts
@@ -1,14 +1,14 @@
-import { IComment } from './comment.interface';
+import { IComment } from './comment.interface'
 
 export interface IMemory {
-  userId: string | null | undefined;
-  username: string | null | undefined;
-  profileImgUrl?: string | null | undefined;
-  imgUrl: string | null | undefined;
-  title: string | null | undefined;
-  description: string | null | undefined;
-  comments: IComment[] | [] | null | undefined;
-  timePosted: Date | null | undefined;
-  alive: boolean | null | undefined;
-  time: Date | null | undefined;
+  userId: string | null | undefined
+  username: string | null | undefined
+  profileImgUrl?: string | null | undefined
+  imgUrl: string | null | undefined
+  title: string | null | undefined
+  description: string | null | undefined
+  comments: IComment[] | [] | null | undefined
+  timePosted: Date | null | undefined
+  alive: boolean | null | undefined
+  time: Date | null | undefined
 }
