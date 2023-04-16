5da343642af990d712294962bf8d4967671119ce
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-04-16 20:32:44 +0200

    Resolved import errors for userState and userApi

--- libs/app/user-view/data-access/src/user-view.module.ts
+++ libs/app/user-view/data-access/src/user-view.module.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AuthModule } from '@mp/app/auth/data-access';
 import { NgxsModule } from '@ngxs/store';
-import { ProfileState } from './profile.state';
-import { ProfilesApi } from './profiles.api';
+import { UserViewState } from './user-view.state';
+import { UserViewApi } from './user-view.api';
 
 @NgModule({
-  imports: [CommonModule, NgxsModule.forFeature([ProfileState]), AuthModule],
-  providers: [ProfilesApi],
+  imports: [CommonModule, NgxsModule.forFeature([UserViewState]), AuthModule],
+  providers: [UserViewApi],
 })
 export class UserViewModule {}
