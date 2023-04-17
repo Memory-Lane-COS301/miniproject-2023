058831e1cdc780021b59833375c8c4eee577a63b
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-04-17 21:53:03 +0200

    Created library for search-results page

---
+++ libs/app/search-results/data-access/src/search-results.module.ts
@@ -0,0 +1,17 @@
+/*
+    The following code was copied from the profile-view/data-access directory.
+    Uncomment it and update the code such that it does not import the profile-view state and api
+    but rather the new search-results state and api
+*/
+
+// import { CommonModule } from '@angular/common';
+// import { NgModule } from '@angular/core';
+// import { NgxsModule } from '@ngxs/store';
+// import { ProfileViewState } from './profile-view.state';
+// import { ProfileViewApi } from './profile-view.api'
+
+// @NgModule({
+//   imports: [CommonModule, NgxsModule.forFeature([ProfileViewState])],
+//   providers: [ProfileViewApi],
+// })
+// export class ProfileViewModule {}
