dd4b41005d9d51b11678fd24fa923ade6019ef01
AlistairMRoss <alistairmikeross@gmail.com>
2023-04-20 11:14:21 +0200

    Fixing Forgot page Tests

--- libs/app/forgot/feature/src/forgot.page.spec.ts
+++ libs/app/forgot/feature/src/forgot.page.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ForgotPasswordPage } from './forgot.page';
+//import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+//TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
 describe('ForgotPage', () => {
   let component: ForgotPasswordPage;
@@ -9,7 +11,7 @@ describe('ForgotPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ForgotPasswordPage],
-      //schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ForgotPasswordPage);
