816685c1f06f5496464a47cf4e81f34991fe9cf6
AlistairMRoss <alistairmikeross@gmail.com>
2023-04-20 11:49:19 +0200

    Unit test for forgot working

--- libs/app/forgot/feature/src/forgot.page.spec.ts
+++ libs/app/forgot/feature/src/forgot.page.spec.ts
@@ -1,17 +1,29 @@
+import 'zone.js';
+import 'zone.js/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ForgotPasswordPage } from './forgot.page';
-//import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
-//TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+import { NgxsModule } from '@ngxs/store';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
 
 describe('ForgotPage', () => {
   let component: ForgotPasswordPage;
   let fixture: ComponentFixture<ForgotPasswordPage>;
+  //TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+
+  beforeAll(() => {
+    TestBed.initTestEnvironment(
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting()
+    );
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      //teardown: { destroyAfterEach: false },
       declarations: [ForgotPasswordPage],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      imports: [NgxsModule.forRoot([])],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ForgotPasswordPage);
