9a09ef4f78209da6c7a05f47ba45bc4400777efa
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-04-24 17:36:58 +0200

    Added code for html

--- libs/app/notification-page/feature/src/notification-page.page.ts
+++ libs/app/notification-page/feature/src/notification-page.page.ts
@@ -18,7 +18,61 @@ export class NotificationPage {
     @Select(NotificationPageState.friendRequests) friendRequests$!: Observable<IUser[] | null>;
     @Select(NotificationPageState.comments) comments$!: Observable<IComment[] | null>;
 
+    //Mock data for testing html
+    mock_requests : IUser[] = [
+        {
+            userId: "jsdjbsdbjhdsbcjshbdcjbsdchs",
+            username: "John_do3",
+            name: "John",
+            surname: "Doe",
+            profileImgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+        },
+        {
+            userId: "jsdjbsdbjhdsbcjshbdcjbsdchs",
+            username: "John_do3",
+            name: "John",
+            surname: "Doe",
+            profileImgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+        },
+        {
+            userId: "jsdjbsdbjhdsbcjshbdcjbsdchs",
+            username: "John_do3",
+            name: "John",
+            surname: "Doe",
+            profileImgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+        }
+    ];
+
+    mock_comments : IComment[] = [
+        {
+            userId: "jsdjbsdbjhdsbcjshbdcjbsdchs",
+            username: "John_do3",
+            profileImgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+            text: "Example comment jakbhbdcjhsjdcbsjdcb"
+        },
+        {
+            userId: "jsdjbsdbjhdsbcjshbdcjbsdchs",
+            username: "John_do3",
+            profileImgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+            text: "Example comment jakbhbdcjhsjdcbsjdcb"
+        },
+        {
+            userId: "jsdjbsdbjhdsbcjshbdcjbsdchs",
+            username: "John_do3",
+            profileImgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+            text: "Example comment jakbhbdcjhsjdcbsjdcb"
+        },
+    ]
+
     constructor(
         private store: Store
     ) {}
+
+    get FriendRequests() {
+        return this.mock_requests;
+    }
+
+    get Comments() {
+        return this.mock_comments;
+    }
 }
