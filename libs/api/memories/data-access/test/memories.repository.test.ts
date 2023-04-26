06c9ea22c7d1bfa9cedc089d154674ea1d53cad5
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-04-26 20:53:54 +0200

    testing all queries

--- libs/api/memories/data-access/test/memories.repository.test.ts
+++ libs/api/memories/data-access/test/memories.repository.test.ts
@@ -103,6 +103,81 @@ class MockMemoriesRepository {
     db.settings({
       host: 'localhost:5003',
       ssl: false,
-    });
-    //tests
-  }
+    });  const memoriesRepository = new MockMemoriesRepository();
+  const memory = {
+    userId: 'testUserID',
+    username: 'testUsername',
+    imgUrl: 'testImgUrl',
+    profileImgUrl: 'testProfileImgUrl',
+    title: 'testTitle',
+    description: 'test Description',
+    commentsCount: 0,
+    remainingTime: 24 * 60 * 60,
+    alive: true,
+    comments: [],
+  };
+  const memoryId = '00085b00-a9ab-410c-98bb-237a74afa05c'; // seeded memory
+  const comment = {
+    userId: 'testUserID',
+    memoryId: 'testMemoryID',
+    commentId: 'testCommentId',
+    username: 'testCommentUsername',
+    profileImgUrl: 'test comment profileImgUrl',
+    text: 'test comment text',
+  };
+  test(
+    'Testing createMemory',
+    async () => {
+      const writeReponse = await memoriesRepository.createMemory(memory);
+      console.debug('writeReponse. ', writeReponse);
+      expect(writeReponse.writeTime).toBeTruthy;
+    },
+    10 * 1000,
+  );
+// test('Testing findOne',async () => {
+  //     const findOneDocumentSnapshot = await memoriesRepository.findOne(memory);
+  //     console.debug('findOneDocumentSnapshot ', findOneDocumentSnapshot);
+  //     expect(findOneDocumentSnapshot.exists).toBe(true);
+  // });
+
+  test('Testing findMemory', async () => {
+    const findMemoryDocumentSnapshot = (await memoriesRepository.findMemory(memoryId)).data();
+    console.debug('findMemoryDocumentSnapshot ', findMemoryDocumentSnapshot);
+    expect(findMemoryDocumentSnapshot).toBeTruthy;
+  });
+
+  test('Testing getComments', async () => {
+    const fetchedComments = await memoriesRepository.getComments(memoryId);
+    const fetechedCommentIds = [
+      //comments for seeded memory with memoryId^
+      '1e2bc945-3d00-4fd3-8285-a1f20270801d',
+      '1e40c657-d256-4f84-9611-1651f8b32caa',
+      '4bab9804-bc62-40cf-8da6-559bab1206b7',
+      '91e83740-be38-4fd7-ab26-f2631d9f6a15',
+      'a4cfa5f5-c8d1-4868-8363-1cb368952e61',
+    ];
+    console.debug('fetchedComments: ', fetchedComments);
+    const hasAllIds = (comment: IComment) => fetechedCommentIds.includes(comment.commentId!);
+    expect(fetchedComments.every(hasAllIds)).toBe(true);
+  });
+
+  // test('Testing getFeedMemories',async () => {
+  //     const userId = '0104fa66-5a7b-429c-aedd-acab833be72e';
+  //     const feedMemories = (await memoriesRepository.getFeedMemories(userId)).docs;
+  //     console.debug('feedMemories ',feedMemories);
+  //     expect(feedMemories).toBeTruthy;
+  // },10*1000);
+
+  test('Testing createComment', async () => {
+    comment.memoryId = '0023f730-0bbf-4059-b84a-7cd3c608f641'; // seeded memory
+    const createCommentWriteResults = await memoriesRepository.createComment(comment);
+    console.debug('createCommentWriteResults ', createCommentWriteResults);
+    expect(createCommentWriteResults.writeTime).toBeTruthy;
+  });
+
+  test('Testing editComment', async () => {
+    const editCommentResults = await memoriesRepository.editComment(comment);
+    console.debug('editCommentResults ', editCommentResults);
+    expect(editCommentResults).toBe(null);
+  });
+});
